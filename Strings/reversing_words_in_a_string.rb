21f0179d9d2b7a9bf0c1e5aefcef794bb5630770
MarkLombardiNelson <marklombardinelson@gmail.com>
2017-02-09 22:11:17 -0500

    Kata #12

---
+++ Strings/reversing_words_in_a_string.rb
@@ -0,0 +1,7 @@
+# You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+# Happy coding!
+
+def reverse(string)
+  string.split.reverse.join(" ")
+end
